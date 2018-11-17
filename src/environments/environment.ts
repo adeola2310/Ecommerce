bbebff71587758a465f0aebaefb3742d9997e338
adeola2310 <adekoyejoadeola@gmail.com>
2018-11-17 03:03:34 +0100

    ecommrce web app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'https://v3.api.webchoice.ng/api',
+  // API_URL: 'https://api.webchoice.ng/api',
+  // API_URL: 'http://localhost:9000/api',
+  API_VERSION: 'v3',
+  APP_DOMAIN: 'https://webchoice.ng/#'
+  // API_VERSION: 'v1'
 };
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
